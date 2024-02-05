c8def4c845c55dd4312dbbef8eb97409f963bcf6
Crowdbotics <team@crowdbotics.com>
2024-02-05 23:01:07 +0000

    Initial commit

---
+++ backend/hydratehero_46018/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for hydratehero_46018 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "hydratehero_46018.settings"
+)
+
+application = get_wsgi_application()
